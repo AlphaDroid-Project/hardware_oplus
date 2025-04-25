baec13635a54578b64cd6971c31ee6471af2d89a
Bruno Martins <bgcngm@gmail.com>
2025-04-25 15:22:14 +0100

    touch: Migrate to AIDL
    Change-Id: I7fc7992744df049835213c58261eb81579d6f3c1

--- aidl/touch/TouchscreenGesture.h
+++ aidl/touch/TouchscreenGesture.h
@@ -1,47 +1,22 @@
 /*
- * Copyright (C) 2022 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
-#ifdef ENABLE_OPLUSTOUCH
-#include <aidl/vendor/oplus/hardware/touch/IOplusTouch.h>
-#endif
-#include <hidl/MQDescriptor.h>
-#include <hidl/Status.h>
-#include <vendor/lineage/touch/1.0/ITouchscreenGesture.h>
+#include <aidl/vendor/lineage/touch/BnTouchscreenGesture.h>
 #include <map>
 
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
-namespace V1_0 {
-namespace implementation {
 
-#ifdef ENABLE_OPLUSTOUCH
-using ::aidl::vendor::oplus::hardware::touch::IOplusTouch;
-#endif
-using ::android::hardware::Return;
-using ::android::hardware::Void;
-using ::vendor::lineage::touch::V1_0::Gesture;
-
-class TouchscreenGesture : public ITouchscreenGesture {
+class TouchscreenGesture : public BnTouchscreenGesture {
   public:
-    // Methods from ::vendor::lineage::touch::V1_0::ITouchscreenGesture follow.
-    Return<void> getSupportedGestures(getSupportedGestures_cb resultCb) override;
-    Return<bool> setGestureEnabled(const Gesture& gesture, bool enabled) override;
+    ndk::ScopedAStatus getSupportedGestures(std::vector<Gesture>* _aidl_return) override;
+    ndk::ScopedAStatus setGestureEnabled(const Gesture& gesture, bool enabled) override;
 
   private:
     // See: drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_common.h
@@ -95,13 +70,9 @@ class TouchscreenGesture : public ITouchscreenGesture {
         return ((1 << head) | ... | (1 << tail));
     }
     static const int kSupportedGestures;
-#ifdef ENABLE_OPLUSTOUCH
-    std::shared_ptr<IOplusTouch> mTouchService;
-#endif
 };
 
-}  // namespace implementation
-}  // namespace V1_0
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
+}  // namespace aidl
